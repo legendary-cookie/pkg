4bf9c7a9d064be0a83f066c9289e92c88eb4f650
cook1e <vincent.schweiger@xolley.de>
2021-05-07 14:19:12 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("pkg-cosmo")
+        .about("A package manager nobody asked for")
+        .version("1.0")
+        .author("Vento")
+        .subcommand(
+            App::new("repo-add").about("adds a new repository").about("Add a repo").arg(
+                Arg::new("URL")
+                    .about("repository URL to add")
+                    .required(true),
+            ),
+        )
+        .get_matches();
+
+    if let Some(add_matches) = matches.subcommand_matches("repo-add") {
+        // Now we have a reference to clone's matches
+        println!("Adding repo: {}", add_matches.value_of("URL").unwrap());
+    }
+}
