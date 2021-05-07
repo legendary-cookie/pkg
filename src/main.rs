559a49af3481274f64d774da4ed0d3791bb79b8d
cook1e <vincent.schweiger@xolley.de>
2021-05-07 14:41:05 +0200

    some stuff

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ fn main() {
         .version("1.0")
         .author("Vento")
         .subcommand(
-            App::new("repo-add").about("adds a new repository").about("Add a repo").arg(
-                Arg::new("URL")
-                    .about("repository URL to add")
-                    .required(true),
-            ),
+            App::new("repo-add")
+                .about("adds a new repository")
+                .about("Add a repo")
+                .arg(
+                    Arg::new("URL")
+                        .about("repository URL to add")
+                        .required(true),
+                ),
         )
         .get_matches();
 
     if let Some(add_matches) = matches.subcommand_matches("repo-add") {
         // Now we have a reference to clone's matches
-        println!("Adding repo: {}", add_matches.value_of("URL").unwrap());
+        let repo = add_matches.value_of("URL").unwrap();
+        println!("Adding repo: {}", repo);
     }
 }
